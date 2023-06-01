b1b0e6262bc450829fa647de59af054d4382e808
HELOUISE DAYANE DOS SANTOS SILVA <109963008+HelouiseDayane@users.noreply.github.com>
2023-05-31 21:50:52 -0300

    front em react

--- webchat_helouise/routes/api.php
+++ webchat_helouise/routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+ 
 Route::apiResource('conversas', ConversaController::class);
 Route::apiResource('mensagems', MensagemController::class);
